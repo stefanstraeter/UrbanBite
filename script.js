69356a40483b6864a63405910e2002dab5060748
stefanstraeter <stefan.straeter@gmail.com>
2025-10-20 09:18:39 +0200

    style: add Figtree font

---
+++ script.js
@@ -0,0 +1,12 @@
+// sticky warenkorb
+// warenkorb anzahl der Gerichte über + und - erhöhen/zureduzieren
+// wenn nur ein Gericht im Warenkorb ist, über - es aus dem Warenkorb löschen
+// Löschen Button im Warenkorb
+// Responsive Warenkorb
+
+// Lieferkosten mit Switch für Liefern - Abholung, bei Lierfung werden 5€ Lieferkosten hinzugefügt
+
+// Hauptgericht, Beilagen, Getränke, etc. Kategorien
+
+// JSON Array Grundstuktur Preis ist Number 9.50 -> 9,50€
+// Amount könnte man schon mit zum Array hinzufügen vs neues Array für Warenkorb -> mit map() arbeiten 
