e6eca909227965aac48301f154426fcf8fbc284f
stefanstraeter <stefan.straeter@gmail.com>
2025-11-02 13:55:58 +0100

    initial commit: add db.js

---
+++ scripts/db.js
@@ -0,0 +1,160 @@
+let dishes = [
+    {
+        category: "Bento Boxes",
+        items: [
+            {
+                id: "bento-teriyaki",
+                name: "Tokyo Teriyaki Chicken Bento",
+                price: 12.50,
+                description: "Grilled chicken glazed with homemade teriyaki sauce, jasmine rice, edamame & pickled veggies",
+                src: "assets/img/tokyo_teriyaki_chicken_bento.jpg"
+
+            },
+            {
+                id: "bento-salmon",
+                name: "Salmon Shogun Bento",
+                price: 13.90,
+                description: "Pan-seared salmon with miso sesame sauce, rice & sautéed greens",
+                src: "assets/img/salmon_shogun_bento.jpg"
+            },
+            {
+                id: "bento-katsu",
+                name: "Katsu Curry Bento",
+                price: 12.90,
+                description: "Crispy chicken cutlet with rich Japanese curry and steamed rice",
+                src: "assets/img/katsu_curry_bento.jpg"
+            },
+            {
+                id: "bento-veggie",
+                name: "Veggie Samurai Bento",
+                price: 11.50,
+                description: "Tempura vegetables, tofu, soba noodles & citrus ponzu dressing",
+                src: "assets/img/veggie_samurai_bento.jpg"
+            },
+            {
+                id: "bento-yakiniku",
+                name: "Beef Yakiniku Bento",
+                price: 13.50,
+                description: "Marinated beef strips with peppers, onions & soy-sesame glaze",
+                src: "assets/img/beef_yakiniku_bento.jpg"
+            },
+
+        ]
+    },
+    {
+        category: "Noodles & Bowls",
+        items: [
+            {
+                id: "noodle-drift",
+                name: "Ramen Drift",
+                price: 13.90,
+                description: "Shoyu ramen with slow-braised pork belly, egg, scallions & bamboo shoots",
+                src: "assets/img/ramen_drift.jpg"
+            },
+            {
+                id: "noodle-miso",
+                name: "Miso Soul Bowl",
+                price: 11.90,
+                description: "Vegan miso ramen with tofu, shiitake mushrooms & sesame oil",
+                src: "assets/img/miso_soul_bowl.jpg"
+            },
+            {
+                id: "noodle-udon",
+                name: "Udon Street Stir-Fry",
+                price: 11.50,
+                description: "Wok-tossed udon noodles with veggies & teriyaki sauce",
+                src: "assets/img/udon_street_stir_fry.jpg"
+            },
+            {
+                id: "noodle-tantanmen",
+                name: "Spicy Tantanmen",
+                price: 13.50,
+                description: "Spicy sesame-chili ramen with ground pork & bok choy",
+                src: "assets/img/spicy_tantanmen.jpg"
+            },
+            {
+                id: "noodle-yakisoba",
+                name: "Tokyo Yakisoba",
+                price: 12.50,
+                description: "Stir-fried wheat noodles with cabbage, carrots & bonito flakes",
+                src: "assets/img/tokyo_yakisoba.jpg"
+            },
+
+        ]
+    },
+    {
+        category: "Sushi",
+        items: [
+            {
+                id: "sushi-drift",
+                name: "Drift Roll",
+                price: 8.50,
+                description: "Inside-out roll with salmon, avocado & chili mayo",
+                src: "assets/img/drift_roll.JPG"
+            },
+            {
+                id: "sushi-tempura",
+                name: "Tokyo Tempura Roll",
+                price: 9.00,
+                description: "Shrimp tempura, cucumber & teriyaki glaze",
+                src: "assets/img/tokyo_tempura_roll.JPG"
+            },
+            {
+                id: "sushi-spicy-tuna",
+                name: "Spicy Tuna Roll",
+                price: 9.50,
+                description: "Tuna, chili, scallion & sesame",
+                src: "assets/img/spicy_tuna_roll.JPG"
+            },
+            {
+                id: "sushi-veggie",
+                name: "Veggie Maki",
+                price: 7.90,
+                description: "Cucumber, avocado, bell pepper & sesame rice",
+                src: "assets/img/veggie_maki.JPG"
+            },
+            {
+                id: "sushi-gyoza",
+                name: "Gyoza Mix",
+                price: 8.90,
+                description: "Homemade dumplings (chicken, veggie or shrimp)",
+                src: "assets/img/gyoza_mix.JPG"
+            },
+
+        ]
+    },
+    {
+        category: "Drinks",
+        items: [
+            {
+                id: "drink-matcha",
+                name: "Matcha Latte",
+                price: 4.50,
+                description: "Creamy green tea latte with earthy matcha",
+                src: "assets/img/matcha_latte.jpg"
+            },
+            {
+                id: "drink-lemonade",
+                name: "Yuzu Lemonade",
+                price: 4.00,
+                description: "Sparkling citrus lemonade with Japanese yuzu",
+                src: "assets/img/yuzu_lemonade.jpg"
+            },
+            {
+                id: "drink-Soda",
+                name: "Ramune Soda",
+                price: 3.90,
+                description: "Iconic Japanese marble soda in assorted flavors",
+                src: "assets/img/ramune_soda.jpg"
+            },
+            {
+                id: "drink-tea",
+                name: "Iced Sakura Tea",
+                price: 4.20,
+                description: "Refreshing cherry blossom tea with a hint of honey",
+                src: "assets/img/sakura_tea.jpg"
+            },
+
+        ]
+    }
+];
